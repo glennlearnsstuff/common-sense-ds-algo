3810d5f29dbdfaa52402d4f81c3f68bc327f7527
glennlearnsstuff <21083722+glennlearnsstuff@users.noreply.github.com>
2022-02-21 23:51:14 +0000

    add fields

--- leetcode/src/TwoSum.cs
+++ leetcode/src/TwoSum.cs
@@ -2,7 +2,7 @@
 
 public class TwoSum
 {
-    public int[] Find(int[] array, int target)
+    public (bool, int[]?) Find(int[] array, int target)
     {
         var cache = new Dictionary<int, int>();
         for (var i = 0; i < array.Length; i++)
@@ -11,10 +11,10 @@ public class TwoSum
             var second = target - first;
             if (cache.TryGetValue(second, out int index))
             {
-                return new int[] { index, i };
+                return (true, new int[] { index, i });
             }
             cache[first] = i;
         }
-        return new int[] { 0, 0 };
+        return (false, null);
     }
 }
--- leetcode/tests/TwoSumTests.cs
+++ leetcode/tests/TwoSumTests.cs
@@ -5,13 +5,14 @@ namespace Leetcode.Tests;
 public class TwoSumTests
 {
     [Theory]
-    [InlineData(new int[] { 2, 7, 11, 15 }, 9, new int[] { 0, 1 })]
-    [InlineData(new int[] { 3, 2, 4 }, 6, new int[] { 1, 2 })]
-    [InlineData(new int[] { 3, 3 }, 6, new int[] { 0, 1 })]
-    public void FindTest(int[] input, int target, int[] correctIndex)
+    [InlineData(new int[] { 2, 7, 11, 15 }, 9, true, 0, 1)]
+    [InlineData(new int[] { 3, 2, 4 }, 6, true, 1, 2)]
+    [InlineData(new int[] { 3, 3 }, 6, true, 0, 1)]
+    public void FindTest(int[] input, int target, bool found, int indexOne, int indexTwo)
     {
-        var twoSum = new TwoSum();
-        var result = twoSum.Find(input, target);
-        Assert.Equal(correctIndex, result);
+        var expected = (Item1: found, new int[] { indexOne, indexTwo });
+        var actual = new TwoSum().Find(input, target);
+        Assert.Equal(expected.Item1, actual.Item1);
+        Assert.Equal(expected.Item2, actual.Item2);
     }
 }
