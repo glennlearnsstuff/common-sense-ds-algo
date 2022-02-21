d8e89c8f4af68b55196a2cf6cca275d61c4c2e1f
glennlearnsstuff <21083722+glennlearnsstuff@users.noreply.github.com>
2022-02-21 18:53:45 -0500

    TwoSum (#2)

---
+++ leetcode/src/TwoSum.cs
@@ -0,0 +1,20 @@
+﻿namespace Leetcode;
+
+public class TwoSum
+{
+    public (bool, int[]?) Find(int[] array, int target)
+    {
+        var cache = new Dictionary<int, int>();
+        for (var i = 0; i < array.Length; i++)
+        {
+            var first = array[i];
+            var second = target - first;
+            if (cache.TryGetValue(second, out int index))
+            {
+                return (true, new int[] { index, i });
+            }
+            cache[first] = i;
+        }
+        return (false, null);
+    }
+}
